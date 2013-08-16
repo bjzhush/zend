b944b9b66bf38a83f617e53151757a2056f9402d
zhushuai @ burst <bjzhush@gmail.com>
2013-08-16 08:32:32 +0400

    Clen indexController ,delete application.ini

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -4,11 +4,6 @@ class IndexController extends Base_ControllerBase
 
     public function indexAction()
     {
-        $db = Base_Registry::getDb();
-        $all =  $db->fetchAll('select * from user');
-        var_dump($all);
-        exit;
-
     }
 
 
