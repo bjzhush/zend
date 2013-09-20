d4d887af881fc44d52d00c2ec950fc17c295245b
zhushuaiAtMyPc <bjzhush@gmail.com>
2013-09-20 17:46:35 +0800

    Modify Controller Base

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -4,6 +4,8 @@ class IndexController extends Base_ControllerBase
 
     public function indexAction()
     {
+        $db = Base_Registry::getDb();
+        exit;
     }
 
 
--- library/Base/ControllerBase.php
+++ library/Base/ControllerBase.php
@@ -1,7 +1,5 @@
 <?php
 class Base_ControllerBase extends Zend_Controller_Action
 {
-    public static function getDb() {
-    }
 
 }
