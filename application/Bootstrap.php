10953b4fc4a626b1cbfd78139ba2bdfd10aa879e
zhushuaiAtMyPc <bjzhush@gmail.com>
2013-10-15 11:56:53 +0800

    增加自动挂载libiary下面的目录,增加disableview函数

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -5,8 +5,20 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
     public function _initAutoload()
     {
         $loader = Zend_Loader_Autoloader::getInstance();
-        //or for multiple namespaces
-        $loader->registerNamespace(array('Db_', 'Base_'));
+
+        // auto register all nameSpaces in library directory
+        $handle = opendir(dirname(__DIR__).'/library/');
+        $nameSpaceArray = array();
+        while(($file = readdir($handle)) != FALSE) {
+            if (is_dir(dirname(__DIR__).'/library/'.$file) && $file != '.' && $file !='..') {
+                array_push($nameSpaceArray, $file.'_');
+            }
+        }
+        $loader->registerNamespace($nameSpaceArray);
+
+        // load helper
+        Zend_Controller_Action_HelperBroker::addHelper(new Zend_Layout_Controller_Action_Helper_Layout);
+
     }
 
 }
--- library/Base/ControllerBase.php
+++ library/Base/ControllerBase.php
@@ -1,5 +1,10 @@
 <?php
 class Base_ControllerBase extends Zend_Controller_Action
 {
+    public function disableLayoutAndView()
+    {
+        $this->_helper->layout->disableLayout();
+        $this->_helper->viewRenderer->setNoRender();
+    }
 
 }
